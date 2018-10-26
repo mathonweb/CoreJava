2111f2d6c0a3ab2ed17e8be50f9e711909f4abd2
Mathieu Guilbault <mguilbault@gmail.com>
2018-10-25 20:03:33 -0400

    Complete the ch03/Exercise12

--- src/ch03/Exercise12.java
+++ src/ch03/Exercise12.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileFilter;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Collections;
 
 public class Exercise12 {
 
@@ -46,15 +47,21 @@ public class Exercise12 {
     }
 
     // Method to sort the directories and files separately
+    public static ArrayList<File> sortFiles(ArrayList<File> filesList) {
 
         // Lambda for comparison
         Comparator<File> comp = (par1, par2) -> {
-            if(par1.isDirectory() && par2.isDirectory()) {
-                return par1.compareTo(par2);
-            } else if(par1.isFile() && par2.isFile()) {
-                return par1.compareTo(par2);
+            if (par1.isDirectory() && par2.isDirectory()) {
+                return par1.getName().compareTo(par2.getName());
+            } else if (par1.isFile() && par2.isFile()) {
+                return par1.getName().compareTo(par2.getName());
             } else return 0;
         };
+
+        Collections.sort(filesList, comp);
+
+        return filesList;
+    }
 
     public static void main(String[] args) {
         //Exercise #12
@@ -63,11 +70,15 @@ public class Exercise12 {
         // Create the path where the files are located
         File path = new File("C:/Users/Mathieu/Projets/Training/CoreJava/");
 
-        // Get all directories and file in the path
-        ArrayList dirFilesList = findFilesDir(path);
+        // Get all directories and files in the path
+        ArrayList<File> dirFilesList = findFilesDir(path);
 
         // Sort by pathname
+        ArrayList<File> filesListSorted = sortFiles(dirFilesList);
 
         // Print the result
+        for(int i=0; i < filesListSorted.size(); i++) {
+            System.out.println(filesListSorted.get(i));
+        }
     }
 }
