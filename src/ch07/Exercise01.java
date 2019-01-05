e08e3fbc1a99e61db9be990804c2458ddb68e199
Mathieu Guilbault <mguilbault@gmail.com>
2019-01-05 14:44:13 -0500

    Add BitSet for ch07 Exercice01

--- src/ch07/Exercise01.java
+++ src/ch07/Exercise01.java
@@ -1,14 +1,24 @@
 package ch07;
 
+import java.util.BitSet;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Exercise01 {
     // Create the Set of numbers
-    private static Set<Integer> createSet(int n){
+    private static Set<Integer> createHashSet(int n){
         Set<Integer> numbersSet = new HashSet<>();
         for(int index = 2 ; index < n; index++){
             numbersSet.add(index);
+        }
+        return numbersSet;
+    }
+
+    // Create the BitSet
+    private static BitSet createBitSet(int nbits){
+        BitSet numbersSet = new BitSet();
+        for(int index = 2 ; index < nbits; index++){
+            numbersSet.set(index, true);
         }
         return numbersSet;
     }
@@ -18,7 +28,7 @@ public class Exercise01 {
         int smallestValue = 0;
         int index = 0;
 
-        while(Math.pow(smallestValue,2) < n){
+        while(Math.pow(smallestValue,2) <= n){
             smallestValue = (Integer)numbersSet.toArray()[index];
 
             // Look for value associated to s^2, s*(s+1), s*(s+2), and so on
@@ -34,17 +44,37 @@ public class Exercise01 {
         return numbersSet;
     }
 
+    // Get the Prime bit
+    private static BitSet getPrimeBit(BitSet numberSet, int n){
+        int index = 2 ;
+
+        while(Math.pow(index,2) <= n) {
+            for (int incr = index; (index*incr) <= n; incr++){
+                numberSet.set(index*incr,false);
+            }
+            index ++;
+        }
+        return numberSet;
+    }
+
     public static void main(String[] args) {
         //Exercise #01
         System.out.println("Exercise #01");
         System.out.println("Sieve of Eratosthenes");
 
-        Set<Integer> numberSet = createSet(100);
+        Set<Integer> numberSet = createHashSet(100);
         Set<Integer> primeNumbers = getPrimeNumbers(numberSet, 100);
+
+        BitSet numberBitSet = createBitSet(100);
+        BitSet primeNumbersBitSet = getPrimeBit(numberBitSet, 100);
 
         // Print all Prime numbers
         for(Integer element : primeNumbers){
             System.out.println(element.toString());
         }
+
+        // Print all Prime numbers bits
+        // It is represented by the True bit position.  Cleared bits are not shown.
+        System.out.println(primeNumbersBitSet.get(0, 99));
     }
 }
