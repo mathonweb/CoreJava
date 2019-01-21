2244cd451ef1696b0d934c004cd680dec169f975
Mathieu Guilbault <mguilbault@gmail.com>
2019-01-20 21:29:34 -0500

    Add ch08 Exercise01

---
+++ src/ch08/Exercise01.java
@@ -0,0 +1,27 @@
+package ch08;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Exercise01 {
+    public static void main(String[] args) {
+        //Exercise #01
+        System.out.println("Exercise #01");
+
+        List<String> words = new ArrayList<>(Arrays.asList("one","two","three","four","five","six","seven","eight",
+                "nine","ten"));
+
+        Stream<String> firstFiveLongWord = words.stream()
+                .sorted(Comparator.comparing(String::length).reversed())
+                .limit(5);
+
+        firstFiveLongWord.forEach(System.out::println);
+
+
+    }
+}
